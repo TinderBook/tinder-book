c3a96e50e8feae0183f4dd8df4750e9c40c8c919
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-09-02 20:19:18 -0400

    feat. loadSession & navbar

--- app.js
+++ app.js
@@ -5,19 +5,29 @@ const app = express();
 
 //connect mongo
 require('./config/db.config')
+//connect to hbs
+require('./config/hbs.config')
 
 
 //Configure view engine
 app.set("view engine", "hbs");
 app.set("views", `${__dirname}/views`);
 
+//config body http
 app.use(express.urlencoded({ extended: true}));
 
+//config session archivo y sus middlewares 
 const sessionConfig = require('./config/session.config');
 app.use(sessionConfig.session);
+app.use(sessionConfig.loadSessionUser)
 
 //Configure static fields
 app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    res.locals.navigationPath = req.path
+    next()
+})
 
 //Routes
 const routes = require('./config/routes.config');
