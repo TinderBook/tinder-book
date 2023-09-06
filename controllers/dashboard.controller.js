d5634122e96ef45c358ab2ebc2e543fb59141acf
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-09-05 20:34:04 -0400

    feat. logout y likes rutas

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,11 +1,19 @@
 const User = require('../models/user.model')
+const Like = require('../models/like.model')
 
 module.exports.showUsers = (req, res, next) => {
 
     User.find()
         .populate('likedBooks')
-        .then((users) => {
-            res.render('dashboard', {users: users})
+        .then(users => {
+            return Like.find({ fromUser: req.session.userId })
+            .then(likes => {
+                const likedUserIds = likes.map(like => String(like.toUser));
+                users.forEach(user => {
+                    user.isLiked = likedUserIds.includes(String(user._id));
+                })
+                res.render('dashboard', {users})
+            })
         })
         .catch((error) => next(error))
 }
