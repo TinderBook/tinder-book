23b1bf8dd9a77d5862f1934cae8a6dfc2c858b00
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-09-07 17:48:31 -0400

    feat. agregué req.flash middleware en res.locals

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const logger = require('morgan')
+const flash = require('connect-flash')
 
 //connect mongo
 require('./config/db.config')
@@ -15,17 +17,22 @@ app.set("views", `${__dirname}/views`);
 
 //config body http
 app.use(express.urlencoded({ extended: true}));
+//connect logger
+app.use(logger('dev'))
+
 
 //config session archivo y sus middlewares 
 const sessionConfig = require('./config/session.config');
 app.use(sessionConfig.session);
 app.use(sessionConfig.loadSessionUser)
+app.use(flash());
 
 //Configure static fields
 app.use(express.static("public"));
 
 app.use((req, res, next) => {
     res.locals.navigationPath = req.path
+    res.locals.messages = req.flash();
     next();
 })
 
