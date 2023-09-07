73ff30104647a51fab7d6f439ebeb216223ebb26
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-09-07 14:46:03 -0400

    match created

--- controllers/likes.controller.js
+++ controllers/likes.controller.js
@@ -1,35 +1,40 @@
-const Like = require('../models/like.model.js');
-const User = require('../models/user.model.js');
+const Like = require("../models/like.model.js");
+const User = require("../models/user.model.js");
+const Match = require("../models/match.model.js");
 
 module.exports.likeUser = (req, res, next) => {
-    const fromUser = req.session.userId;
-    const toUser = req.params.id;
+  const fromUser = req.user.id;
+  const toUser = req.params.id;
 
-    Like.findOne({ fromUser: fromUser, toUser: toUser })
-        .then(existingLikes => {
-            if (existingLikes) {
-                return Like.deleteOne({ fromUser: fromUser, toUser: toUser })
-            } else {
-                return Like.create({ fromUser: fromUser, toUser: toUser })
-                    .then(like => {
-                        // existe en el otro sentido?
-                        return Like.findOne({ fromUser: toUser, toUser: fromUser })
-                            // si existe, creo el match
-                            .then(existingLike => {
-                                // redirijo al dashboard con un query param para mostrar un mensaje
-                                if (existingLike) {
-                                    return User.createMatch(fromUser, toUser)
-                                }
-
-                            })
-                    })
-            }
-        })
-        .then(() => {
-            res.redirect('/dashboard')
-        })
-        .catch(error => {
-            next(error)
-        })
-
-}
+  Like.findOne({ fromUser: fromUser, toUser: toUser })
+    .then((existingLikes) => {
+      if (existingLikes) {
+        return Like.deleteOne({ fromUser: fromUser, toUser: toUser });
+      } else {
+        return Like.create({ fromUser: fromUser, toUser: toUser }).then(
+          (like) => {
+            // existe en el otro sentido?
+            return (
+              Like.findOne({ fromUser: toUser, toUser: fromUser })
+                // si existe, creo el match
+                .then((existingLike) => {
+                  // redirijo al dashboard con un query param para mostrar un mensaje
+                  if (existingLike) {
+                    return Match.create({
+                      user1: toUser,
+                      user2: fromUser,
+                    });
+                  }
+                })
+            );
+          }
+        );
+      }
+    })
+    .then(() => {
+      res.redirect("/dashboard");
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
