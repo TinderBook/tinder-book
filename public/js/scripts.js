027c56964fc9f2b25e10cbb871480af964b966f8
Driztnafein <127021511+Driztnafein@users.noreply.github.com>
2023-09-11 20:39:54 +0100

    chat

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,5 +10,5 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function() {
     setInterval(function() {
         location.reload();
-    }, 3000); // Refresca la página cada 3 segundos
+    }, 10000); // Refresca la página cada 3 segundos
 });
