500099210da9ba95d6c6f1a100487199699f8c54
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-09-12 19:05:53 -0400

    algunos cambio cristian recomienda en css y arreglos

--- controllers/match.controller.js
+++ controllers/match.controller.js
@@ -2,7 +2,7 @@ const Match = require('../models/match.model');
 
 module.exports = {
     viewMyMatches: (req, res, next) => {
-        const userId = req.session.userId;
+        const userId = req.user.id;
         Match.find({ $or: [{ user1: userId }, { user2: userId }] })
             .populate('user1 user2')
             .then(matches => {
