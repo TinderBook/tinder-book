ed54ae5e1d3c38a13dbe24fdfeb0792bcd4bee3c
Driztnafein <127021511+Driztnafein@users.noreply.github.com>
2023-09-13 18:35:14 +0100

    register vista

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -10,6 +10,7 @@ module.exports.register = (req, res, next) => {
 }
 
 module.exports.doRegister = (req, res, next) => {
+    // Verifica si el username ya existe
     User.findOne({ username: req.body.username })
         .then((user) => {
             if (user) {
@@ -22,18 +23,34 @@ module.exports.doRegister = (req, res, next) => {
                                 username: 'Username already exists',
                             }
                         })
-
                     })
             } else {
-                return User.create(req.body)
-                    .then(() => {
-                        res.redirect('/login')
-                    })
+                // Si el username no existe, verifica si el email ya está en uso
+                return User.findOne({ email: req.body.email })
+                    .then(userWithEmail => {
+                        if (userWithEmail) {
+                            return Book.find()
+                                .then((books) => {
+                                    res.render('users/register', {
+                                        user: req.body,
+                                        books: books,
+                                        errors: {
+                                            email: 'Email already in use',
+                                        }
+                                    });
+                                });
+                        } else {
+                            // Si el email tampoco está en uso, crea el usuario
+                            return User.create(req.body)
+                                .then(() => {
+                                    res.redirect('/login')
+                                });
+                        }
+                    });
             }
         })
         .catch((error) => {
             if (error instanceof mongoose.Error.ValidationError) {
-
                 Book.find()
                     .then((books) => {
                         res.render('users/register', {
@@ -48,6 +65,7 @@ module.exports.doRegister = (req, res, next) => {
             }
         })
 }
+
 
 module.exports.login = (req, res, next) => {
     res.render('users/login')
@@ -87,10 +105,10 @@ module.exports.doLogin = (req, res, next) => {
 module.exports.profile = (req, res, next) => {
     User.findById(req.user)
         .populate('likedBooks')
-        .then( user => {
+        .then(user => {
             res.render('users/profile', { user: user })
         })
-        .catch( error => next(error))
+        .catch(error => next(error))
 }
 
 
@@ -98,10 +116,10 @@ module.exports.editDescription = (req, res, next) => {
     User.findByIdAndUpdate(req.params.id, {
         description: req.body.description,
     }, { new: true })
-    .then(() => {
-        res.redirect('/user/profile')
-    })
-    .catch((error) => next(error))
+        .then(() => {
+            res.redirect('/user/profile')
+        })
+        .catch((error) => next(error))
 }
 
 // Renderiza pagina de editar profile 
@@ -109,25 +127,25 @@ module.exports.editProfile = (req, res, next) => {
 
     User.findById(req.user)
 
-        .then( user => {
-            
+        .then(user => {
+
             return Book.find()
                 .then((books) => {
 
-                    res.render('users/edit', {user, books});
+                    res.render('users/edit', { user, books });
 
-            });
+                });
         })
-            .catch((error) => next(error))
+        .catch((error) => next(error))
 
 }
 
 // doEdit profile
 module.exports.doEditProfile = (req, res, next) => {
 
-    User.findOne({username: req.body.username})
+    User.findOne({ username: req.body.username })
         .then(existingUser => {
-            if(existingUser && String(existingUser._id)  !== String(req.params.id)){
+            if (existingUser && String(existingUser._id) !== String(req.params.id)) {
 
                 return Book.find()
                     .then(books => {
@@ -141,28 +159,28 @@ module.exports.doEditProfile = (req, res, next) => {
                     })
             } else {
 
-                req.body.likedBooks =  typeof req.body.likedBooks === 'string' ? [req.body.likedBooks] : req.body.likedBooks
+                req.body.likedBooks = typeof req.body.likedBooks === 'string' ? [req.body.likedBooks] : req.body.likedBooks
 
                 let updates = {
                     username: req.body.username,
                     description: req.body.description,
                     likedBooks: req.body.likedBooks
                 };
-                return User.findByIdAndUpdate(req.params.id, updates, {new: true, runValidators:true})
+                return User.findByIdAndUpdate(req.params.id, updates, { new: true, runValidators: true })
                     .then(() => res.redirect("/user/profile"));
             }
         })
         .catch(error => {
-            if(error instanceof mongoose.Error.ValidationError){
+            if (error instanceof mongoose.Error.ValidationError) {
                 Book.find()
-                 .then(books => {
-                    res.render('users/edit', {
-                        user: req.body,
-                        errors: error.errors,
-                        books: books
-                    });
-                 })
-                 .catch(error => next(error))
+                    .then(books => {
+                        res.render('users/edit', {
+                            user: req.body,
+                            errors: error.errors,
+                            books: books
+                        });
+                    })
+                    .catch(error => next(error))
             } else {
                 next(error);
             }
