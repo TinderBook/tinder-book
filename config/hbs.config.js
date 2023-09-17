26761556159fb766fc877f6e26a190b4c6bc1dd3
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-09-18 00:29:41 +0200

    feat. agregue helper para hacer moment

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -1,12 +1,17 @@
 const hbs = require('hbs');
+const moment = require('moment')
 
-
+//registro de partials en hbs con el folder partials 
 hbs.registerPartials(`${__dirname}/../views/partials`);
 
+
+// este nos dice si la barra del navegador es igual a la q estabamos buscando: nos regresa un string llamado "active"
 hbs.registerHelper('navActive', (navigationPath, expectedPath, options) => {
     return (navigationPath === expectedPath) ? 'active' : "";
 })
 
+
+// este nos dice val1 y val2 son iguales nos regresa el primer options.fn(this) , si no la option.inverse como segunda opcion
 hbs.registerHelper('ifEqual', function (val1, val2, options) {
     if (val1.toString() === val2.toString()) {
         return options.fn(this);  
@@ -15,6 +20,7 @@ hbs.registerHelper('ifEqual', function (val1, val2, options) {
     }
 });
 
+//esta nos dice lo mismo q la anterior pero la estamos usando para comparar si el usuario registrado es el mismo 
 hbs.registerHelper('ifUser', function (val1, val2, options) {
     if (val1.toString() === val2.toString()) {
         return options.fn(this);  
@@ -34,3 +40,7 @@ hbs.registerHelper('formatDate', function(date) {
 hbs.registerHelper('eq', function(a, b) {
     return a === b;
 });
+
+hbs.registerHelper('elapsedTime', (createdAt, options) => {
+    return moment(createdAt).fromNow();
+} )
