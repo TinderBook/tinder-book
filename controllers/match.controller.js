309778bd65e3966c78649ad19524eed8395de836
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-09-18 12:23:19 +0200

    tuneup

--- controllers/match.controller.js
+++ controllers/match.controller.js
@@ -1,58 +1,62 @@
 const Match = require('../models/match.model');
 
 module.exports = {
-    viewMyMatches: (req, res, next) => {
-        const userId = req.user.id;
-        Match.find({ $or: [{ user1: userId }, { user2: userId }] })
-            .populate({
-                path: 'user1',
-                populate: { path: 'likedBooks' }
-            })
-            .populate({
-                path: 'user2',
-                populate: { path: 'likedBooks' }
-            })
-            .then(matches => {
-                const matchedUsers = matches
-                    .filter(match => match.user1 && match.user2)  // Filtramos los matches donde ambos usuarios existen
-                    .map(match => {
-                        let user = match.user1._id.toString() === userId ? match.user2 : match.user1;
-                        return {
-                            user: user,
-                            matchId: match._id.toString()
-                        };
-                    });
-                res.render('users/my-matches', { matches: matchedUsers });
-            })
-            .catch(err => next(err));
-    },
-    
+  viewMyMatches: (req, res, next) => {
+    const userId = req.user.id;
+    Match.find({ $or: [{ user1: userId }, { user2: userId }] })
+      .populate({
+        path: 'user1',
+        populate: { path: 'likedBooks' }
+      })
+      .populate({
+        path: 'user2',
+        populate: { path: 'likedBooks' }
+      })
+      .then(matches => {
+        const matchedUsers = matches
+          .filter(match => match.user1 && match.user2)  // Filtramos los matches donde ambos usuarios existen
+          .map(match => {
+              let user = match.user1._id.toString() === userId ? match.user2 : match.user1;
+              return {
+                  user: user,
+                  matchId: match._id.toString()
+              };
+          });
+        if(matchedUsers.length === 0){
+          return res.render('users/my-matches', { noMatches: true })
+        }  
 
-    matchDetails: (req, res, next) => {
-        Match.findById(req.params.id)
-            .populate('user1')
-            .populate('user2')
-            .populate({
-                path: 'messages',
-                populate: {
-                    path: 'sender',
-                    model: 'User'
-                }
-            })
-            .then(match => {
-                let otherUser = match.user1._id.toString() === req.session.userId ? match.user2 : match.user1;
-                
-                // Update the match notified status to true
-                if (!match.notified) {
-                    match.notified = true;
-                    return match.save()
-                        .then(() => {
-                            res.render("matches/details", { match, otherUser });
-                        });
-                } else {
+        res.render('users/my-matches', { matches: matchedUsers });
+        
+      })
+        .catch(err => next(err));
+  },
+  
+
+  matchDetails: (req, res, next) => {
+    Match.findById(req.params.id)
+      .populate('user1')
+      .populate('user2')
+      .populate({
+        path: 'messages',
+        populate: {
+            path: 'sender',
+            model: 'User'
+        }
+      })
+      .then(match => {
+        let otherUser = match.user1._id.toString() === req.session.userId ? match.user2 : match.user1;
+
+        if (!match.notified) {
+            match.notified = true;
+            return match.save()
+                .then(() => {
                     res.render("matches/details", { match, otherUser });
-                }
-            })
-            .catch(err => next(err));
-    }
+                });
+        } else {
+            res.render("matches/details", { match, otherUser });
+        }
+      })
+      .catch(err => next(err));
+  }
 }
