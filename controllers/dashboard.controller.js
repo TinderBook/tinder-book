b44728d1023d1c7d9b4a2a914f32ecb1941dad03
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-09-19 18:22:26 +0200

    usuarios aleatorios funcion hecha en dashboard controller

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,19 +1,50 @@
 const User = require('../models/user.model')
 const Like = require('../models/like.model')
 
+
+function findRandomUser() {
+    return User.countDocuments({})
+        .then(count => {
+            const random = Math.floor(Math.random() * count);
+            return User.findOne()
+                .populate('likedBooks')  
+                .skip(random)
+                .exec();
+        });
+}
+
+
 module.exports.showUsers = (req, res, next) => {
-    User.find()
-        .populate('likedBooks')
-        .then(users => {
-            return Like.find({ fromUser: req.session.userId })
-            .then(likes => {
-                const likedUserIds = likes.map(like => String(like.toUser));
-                users.forEach(user => {
-                    user.isLiked = likedUserIds.includes(String(user._id));
-                })
-                res.render('dashboard', {users})
-            })
+    const numRandomUsers = 10;
+    const users = [];
+
+    function addRandomUser() {
+        return findRandomUser()
+            .then(user => {
+                if (!users.some(existingUser => String(existingUser._id) === String(user._id))) {
+                    users.push(user);
+                    if (users.length < numRandomUsers) {
+                        return addRandomUser();  
+                    }
+                } else {
+                    return addRandomUser();  
+                }
+            });
+    }
+
+    addRandomUser()
+        .then(() => {
+            return Like.find({ fromUser: req.session.userId });
         })
-        .catch((error) => next(error))
-        
+        .then(likes => {
+            const likedUserIds = likes.map(like => String(like.toUser));
+            users.forEach(user => {
+                user.isLiked = likedUserIds.includes(String(user._id));
+            });
+            res.render('dashboard', { users });
+        })
+        .catch(error => {
+            console.error("Error mostrando usuarios aleatorios:", error);
+            next(error);
+        });
 }
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,197 +3,194 @@ const mongoose = require("mongoose");
 const Book = require("../models/book.model");
 
 module.exports.register = (req, res, next) => {
-    Book.find()
-        .then((books) => res.render('users/register', { books }))
-        .catch((error) => next(error))
+  Book.find()
+    .then((books) => res.render('users/register', { books }))
+    .catch((error) => next(error))
 
 }
 
 module.exports.doRegister = (req, res, next) => {
-    User.findOne({ username: req.body.username })
-        .then((user) => {
-            if (user) {
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      if (user) {
+        return Book.find()
+            .then((books) => {
+                res.render('users/register', {
+                    user: req.body,
+                    books: books,
+                    errors: {
+                        username: 'Username already exists',
+                    }
+                })
+            })
+    } else {
+        return User.findOne({ email: req.body.email })
+            .then(userWithEmail => {
+              if (userWithEmail) {
                 return Book.find()
                     .then((books) => {
                         res.render('users/register', {
                             user: req.body,
                             books: books,
                             errors: {
-                                username: 'Username already exists',
+                                email: 'Email already in use',
                             }
-                        })
-                    })
-            } else {
-                return User.findOne({ email: req.body.email })
-                    .then(userWithEmail => {
-                        if (userWithEmail) {
-                            return Book.find()
-                                .then((books) => {
-                                    res.render('users/register', {
-                                        user: req.body,
-                                        books: books,
-                                        errors: {
-                                            email: 'Email already in use',
-                                        }
-                                    });
-                                });
-                        } else {
-                            // Convert likedBooks to an array if it's not
-                            req.body.likedBooks = typeof req.body.likedBooks === 'string' ? [req.body.likedBooks] : req.body.likedBooks;
-
-                            return User.create(req.body)
-                                .then(() => {
-                                    res.redirect('/login')
-                                });
-                        }
+                        });
                     });
-            }
-        })
-        .catch((error) => {
-            if (error instanceof mongoose.Error.ValidationError) {
-                Book.find()
-                    .then((books) => {
-                        res.render('users/register', {
-                            user: req.body,
-                            books: books,
-                            errors: error.errors
-                        })
-                    })
-                    .catch((error) => next(error));
             } else {
-                next(error)
-            }
-        })
+              
+              req.body.likedBooks = typeof req.body.likedBooks === 'string' ? [req.body.likedBooks] : req.body.likedBooks;
+
+              return User.create(req.body)
+                .then(() => {
+                    res.redirect('/login')
+                });
+              }
+            });
+      }
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        Book.find()
+          .then((books) => {
+            res.render('users/register', {
+              user: req.body,
+              books: books,
+              errors: error.errors
+            })
+          })
+          .catch((error) => next(error));
+      } else {
+          next(error)
+      }
+    })
 }
 
 
 
 module.exports.login = (req, res, next) => {
-    res.render('users/login')
+  res.render('users/login')
 }
 
 module.exports.doLogin = (req, res, next) => {
 
-    function renderInvalidUsername() {
-        res.render('users/login', {
-            user: req.body,
-            errors: {
-                password: 'Invalid username or password'
-            }
-        })
-    }
+  function renderInvalidUsername() {
+    res.render('users/login', {
+      user: req.body,
+      errors: {
+          password: 'Invalid username or password'
+      }
+  })
+  }
 
-    User.findOne({ username: req.body.username })
-        .then((user) => {
-            if (user) {
-                return user.checkPassword(req.body.password)
-                    .then((match) => {
-                        if (match) {
-                            req.session.userId = user.id;
-                            res.redirect('/dashboard')
-                        } else {
-                            renderInvalidUsername();
-                        }
-                    })
-            } else {
-                renderInvalidUsername();
-            }
-        })
-        .catch((error) => next(error));
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      if (user) {
+        return user.checkPassword(req.body.password)
+            .then((match) => {
+              if (match) {
+                  req.session.userId = user.id;
+                  res.redirect('/dashboard')
+              } else {
+                  renderInvalidUsername();
+              }
+            })
+      } else {
+          renderInvalidUsername();
+      }
+  })
+    .catch((error) => next(error));
 }
 
 
 module.exports.profile = (req, res, next) => {
-    User.findById(req.user)
-        .populate('likedBooks')
-        .then(user => {
-            user._id = user._id.toString();
-            res.render('users/profile', { user: user })
-        })
-        .catch(error => next(error))
+  User.findById(req.user)
+    .populate('likedBooks')
+    .then(user => {
+      user._id = user._id.toString();
+      res.render('users/profile', { user: user })
+    })
+    .catch(error => next(error))
 }
 
 
 module.exports.editDescription = (req, res, next) => {
-    User.findByIdAndUpdate(req.params.id, {
-        description: req.body.description,
-    }, { new: true })
-        .then(() => {
-            res.redirect('/user/profile')
-        })
-        .catch((error) => next(error))
+  User.findByIdAndUpdate(req.params.id, {
+      description: req.body.description,
+  }, { new: true })
+    .then(() => {
+        res.redirect('/user/profile')
+    })
+    .catch((error) => next(error))
 }
 
-// Renderiza pagina de editar profile 
+
 module.exports.editProfile = (req, res, next) => {
 
-    User.findById(req.user)
+  User.findById(req.user)
 
-        .then(user => {
+    .then(user => {
 
-            return Book.find()
-                .then((books) => {
-
-                    res.render('users/edit', { user, books });
-
-                });
-        })
-        .catch((error) => next(error))
+      return Book.find()
+        .then((books) => {
+          res.render('users/edit', { user, books });
+        });
+    })
+    .catch((error) => next(error))
 
 }
 
-// doEdit profile
 module.exports.doEditProfile = (req, res, next) => {
 
-    User.findOne({ username: req.body.username })
-        .then(existingUser => {
-            if (existingUser && String(existingUser._id) !== String(req.params.id)) {
+  User.findOne({ username: req.body.username })
+    .then(existingUser => {
+      if (existingUser && String(existingUser._id) !== String(req.params.id)) {
 
-                return Book.find()
-                    .then(books => {
-                        res.render('users/edit', {
-                            user: req.body,
-                            books: books,
-                            errors: {
-                                username: 'username already exists',
-                            }
-                        })
-                    })
-            } else {
-
-                req.body.likedBooks = typeof req.body.likedBooks === 'string' ? [req.body.likedBooks] : req.body.likedBooks
-
-                let updates = {
-                    username: req.body.username,
-                    description: req.body.description,
-                    likedBooks: req.body.likedBooks,
-                };
-                if (req.file && req.file.path) {
-                    updates.avatarUrl = req.file.path;
+        return Book.find()
+          .then(books => {
+              res.render('users/edit', {
+                user: req.body,
+                books: books,
+                errors: {
+                  username: 'username already exists',
                 }
-                return User.findByIdAndUpdate(req.params.id, updates, { new: true, runValidators: true })
-                    .then(() => res.redirect("/user/profile"));
-            }
-        })
-        .catch(error => {
-            if (error instanceof mongoose.Error.ValidationError) {
-                Book.find()
-                    .then(books => {
-                        res.render('users/edit', {
-                            user: req.body,
-                            errors: error.errors,
-                            books: books
-                        });
-                    })
-                    .catch(error => next(error))
-            } else {
-                next(error);
-            }
-        });
+              })
+            })
+        } else {
+
+          req.body.likedBooks = typeof req.body.likedBooks === 'string' ? [req.body.likedBooks] : req.body.likedBooks
+
+          let updates = {
+            username: req.body.username,
+            description: req.body.description,
+            likedBooks: req.body.likedBooks,
+          };
+          if (req.file && req.file.path) {
+            updates.avatarUrl = req.file.path;
+          }
+          return User.findByIdAndUpdate(req.params.id, updates, { new: true, runValidators: true })
+            .then(() => res.redirect("/user/profile"));
+        }
+      })
+      .catch(error => {
+        if (error instanceof mongoose.Error.ValidationError) {
+          Book.find()
+            .then(books => {
+              res.render('users/edit', {
+                user: req.body,
+                errors: error.errors,
+                books: books
+              });
+            })
+            .catch(error => next(error))
+        } else {
+          next(error);
+        }
+      });
 }
 
 
 module.exports.logout = (req, res, next) => {
-    req.session.destroy();
-    res.redirect('/')
+  req.session.destroy();
+  res.redirect('/')
 }
